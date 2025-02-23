d168ccd2c0698df6f4db50412361f7bdb29c9b7a
EATSTEAK <me@eatsteak.dev>
2025-02-23 20:24:29 +0900

    feat: popover menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,5 @@
 import logo from "@/assets/logo.svg";
-import { ListLine } from "@/icons/ListLine";
-import {
-  Popover,
-  PopoverClose,
-  PopoverContent,
-  PopoverPortal,
-  PopoverTrigger,
-} from "@radix-ui/react-popover";
+import NavigationPopover from "./NavigationPopover";
 
 export default function Header() {
   return (
@@ -17,45 +10,7 @@ export default function Header() {
           <div className="shrink-0 w-0.5 self-stretch my-4 bg-white/50" />
           <h1 className="font-bold text-lg lg:text-2xl">Road To Pro</h1>
         </a>
-        <Popover>
-          <PopoverTrigger asChild>
-            <button className="cursor-pointer dropdown-btn hover:scale-105 rounded-lg transition-all active:scale-95 active:bg-white/25">
-              <ListLine className="size-12 lg:size-16 text-white" />
-            </button>
-          </PopoverTrigger>
-          <PopoverPortal>
-            <PopoverContent
-              align="end"
-              className="bg-white text-black rounded-2xl p-4 items-start gap-4 w-72"
-            >
-              <div>
-                <h1>Navigation</h1>
-                <button>
-                  <h1>Road To Pro / 2021</h1>
-                  <p>구르다 보면 보이는 것들</p>
-                </button>
-                <div>
-                  <h1>Road To Pro: Discover</h1>
-                  <ul>
-                    <li>
-                      <a href="#project">Project</a>
-                    </li>
-                    <li>
-                      <a href="#planning_design">Planning & Design</a>
-                    </li>
-                    <li>
-                      <a href="#engineering">Engineering</a>
-                    </li>
-                    <li>
-                      <a href="#operation">Operation</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <PopoverClose>X</PopoverClose>
-            </PopoverContent>
-          </PopoverPortal>
-        </Popover>
+        <NavigationPopover />
       </nav>
     </header>
   );
---
+++ src/components/NavigationPopover.tsx
@@ -0,0 +1,67 @@
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverPortal,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import { ListLine } from "@/icons/ListLine";
+import { Close } from "@/icons/Close";
+
+export default function NavigationPopover() {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <button className="cursor-pointer dropdown-btn hover:scale-105 rounded-lg transition-all active:scale-95 active:bg-white/25">
+          <ListLine className="size-12 lg:size-16 text-white" />
+        </button>
+      </PopoverTrigger>
+      <PopoverPortal>
+        <PopoverContent
+          align="end"
+          className="bg-neutral-100 text-neutral-900 rounded-2xl p-2 gap-4 w-72 drop-shadow-lg z-10"
+        >
+          <div className="flex flex-col gap-2 grow">
+            <div className="flex justify-between items-center px-2 pt-2">
+              <h1 className="font-bold">Road To Pro: Discover</h1>
+              <PopoverClose asChild>
+                <Close className="cursor-pointer size-6 text-netural-900 hover:scale-105 hover:bg-black/25 rounded-lg p-1 transition-all active:scale-95" />
+              </PopoverClose>
+            </div>
+            <hr className="border-neutral-400 mx-2" />
+            <div>
+              <ul className="ml-2">
+                <li className="rounded-lg hover:bg-neutral-600/25 transition-all py-1 px-2">
+                  <a href="#project">Project</a>
+                </li>
+                <li className="rounded-lg hover:bg-neutral-600/25 transition-all py-1 px-2">
+                  <a href="#planning_design">Planning & Design</a>
+                </li>
+                <li className="rounded-lg hover:bg-neutral-600/25 transition-all py-1 px-2">
+                  <a href="#engineering">Engineering</a>
+                </li>
+                <li className="rounded-lg hover:bg-neutral-600/25 transition-all py-1 px-2">
+                  <a href="#operation">Operation</a>
+                </li>
+              </ul>
+            </div>
+            <h1 className="text-sm font-bold text-neutral-500 px-2">
+              Previous Events
+            </h1>
+            <hr className="border-neutral-400 mx-2" />
+            <a
+              href="https://event.yourssu.com/2021"
+              target="_blank"
+              className="rounded-lg hover:bg-neutral-600/25 transition-all p-2"
+            >
+              <h1 className="font-bold">Road To Pro / 2021</h1>
+              <p className="text-sm text-neutral-600">
+                구르다 보면 보이는 것들
+              </p>
+            </a>
+          </div>
+        </PopoverContent>
+      </PopoverPortal>
+    </Popover>
+  );
+}
