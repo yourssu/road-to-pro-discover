ab19ab9fec8517902d2b4a14c64f896dc83e3f58
EATSTEAK <me@eatsteak.dev>
2025-03-01 01:43:54 +0900

    feat: install `tailwind-animate`

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import tailwindAnimate from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -45,5 +46,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindAnimate],
 };
