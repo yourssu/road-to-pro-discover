a38e728526c9aee967baed60778a539524a21f1d
EATSTEAK <me@eatsteak.dev>
2025-03-01 14:08:37 +0900

    feat: recruiting link in outro

--- src/components/Outro.tsx
+++ src/components/Outro.tsx
@@ -2,10 +2,10 @@ import { cn } from "@/lib/utils";
 import { HTMLAttributes, useRef } from "react";
 import discover from "@/assets/discover.svg";
 import logo from "@/assets/logo.svg";
-import { IcExternalLinkLine } from "@yourssu/handy-icons-react";
 import { GitHub } from "@/icons/GitHub";
 import { Instagram } from "@/icons/Instagram";
 import { YouTube } from "@/icons/YouTube";
+import { IcExternalLinkLine } from "@yourssu/handy-icons-react";
 
 export default function Outro({
   className,
@@ -33,16 +33,23 @@ export default function Outro({
           <h2 className="font-bold text-lg lg:text-xl">
             발견과 여정의 시작, 우리는 어디를 향해 걸어가고 있나요?
           </h2>
-          <h3 className="mt-4 flex gap-2 items-center text-base lg:text-lg font-bold text-center whitespace-nowrap">
-            <img src={logo} className="size-8 lg:size-12" alt="YOURSSU" />
-            YOURSSU
-          </h3>
+          <a
+            href="https://yourssu.com/recruiting"
+            className="mt-4 flex gap-2 items-center text-base lg:text-lg font-bold text-center whitespace-nowrap opacity-75 hover:scale-105 hover:opacity-100 active:scale-95 transition-all"
+          >
+            <IcExternalLinkLine className="size-8" />
+            YOURSSU 리크루팅 지원하기 (~03.08)
+          </a>
           <p className="text-sm lg:text-base text-center md:w-max md:max-w-screen-md">
             숭실대학교 학생회관 244호 / ⓒ 2025 Yourssu. All rights reserved.
           </p>
           <div className="flex items-center gap-4">
             <a href="https://yourssu.com" target="_blank">
-              <IcExternalLinkLine className="size-10 text-white/75 hover:scale-105 hover:text-white active:scale-95 transition-all -mx-2" />
+              <img
+                src={logo}
+                className="size-8 opacity-75 hover:scale-105 hover:opacity-100 active:scale-95 transition-all"
+                alt="YOURSSU"
+              />
             </a>
             <a href="https://instagram.com/yourssu_official" target="_blank">
               <Instagram className="size-8 text-white/75 hover:scale-105 hover:text-white active:scale-95 transition-all" />
